4cbb6414fd4a0655a7a0b970bb0c18b67adcf59d
juholeinonen <juho.leinonen@aalto.fi>
2018-03-04 14:38:18 +0200

    New script to add symlinks

---
+++ make_symlinks.sh
@@ -0,0 +1,10 @@
+ln -s local /scratch/work/jpleino/kaldi-trunk/egs/sme_LMs/local
+ln -s local /scratch/work/jpleino/kaldi-trunk/egs/sme_F/local
+
+ln -s cmd_AM_sme_F.sh /scratch/work/jpleino/kaldi-trunk/egs/sme_F/cmd.sh
+ln -s path_AM_sme_F.sh /scratch/work/jpleino/kaldi-trunk/egs/sme_F/path.sh
+ln -s run_AM_sme_F.sh /scratch/work/jpleino/kaldi-trunk/egs/sme_F/run.sh
+
+ln -s cmd_AM_sme_F.sh /scratch/work/jpleino/kaldi-trunk/egs/sme_LMs/cmd.sh
+ln -s path_AM_sme_F.sh /scratch/work/jpleino/kaldi-trunk/egs/sme_LMs/path.sh
+ln -s run_LMs_sme_F.sh /scratch/work/jpleino/kaldi-trunk/egs/sme_LMs/run.sh
