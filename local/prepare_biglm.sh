80895a1242e00d77b406920439525f415706ab85
juholeinonen <juho.leinonen@aalto.fi>
2018-04-26 14:23:38 +0300

    small modifications

--- local/prepare_biglm.sh
+++ local/prepare_biglm.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-if [ $# != 1 ]; then
+if [ $# != 2 ]; then
 
   echo "You're doing this wrong"
   exit 1;
 fi
 
 wiki=$1
-
+comtype=$2
 pre_corpus=pre_corpus.txt
 
 # Create language model corpus
@@ -19,9 +19,9 @@ cp $pre_corpus corpus.txt
 sed -n '0~100p' $pre_corpus > variKN_discount.txt
 sed '0~100d' $pre_corpus > corpus_variKN.txt
 
-mkdir corpora
-mv variKN_discount.txt corpora
-mv corpus.txt corpora
-mv corpus_variKN.txt corpora 
+mkdir corpora_${comtype}
+mv variKN_discount.txt corpora_${comtype}
+mv corpus.txt corpora_${comtype}
+mv corpus_variKN.txt corpora_${comtype}
 
 rm $pre_corpus
--- make_symlinks.sh
+++ make_symlinks.sh
@@ -1,12 +1,12 @@
 #ln -s ${PWD}/local /scratch/work/jpleino1/kaldi-trunk/egs/sme_LMs/local
-ln -s ${PWD}/local /scratch/work/jpleino1/kaldi-trunk/egs/sme_fintinyF/local
-ln -s ${PWD}/conf /scratch/work/jpleino1/kaldi-trunk/egs/sme_fintinyF/conf
+ln -s ${PWD}/local /scratch/work/jpleino1/kaldi-trunk/egs/est_LMs/local
+ln -s ${PWD}/conf /scratch/work/jpleino1/kaldi-trunk/egs/est_LMs/conf
 #ln -s ${PWD}/conf /scratch/work/jpleino1/kaldi-trunk/egs/sme_LMs/conf
 
-ln -s ${PWD}/cmd_AM_sme_F.sh /scratch/work/jpleino1/kaldi-trunk/egs/sme_fintinyF/cmd.sh
-ln -s ${PWD}/path_AM_sme_F.sh /scratch/work/jpleino1/kaldi-trunk/egs/sme_fintinyF/path.sh
-ln -s ${PWD}/run_AM_sme_F.sh /scratch/work/jpleino1/kaldi-trunk/egs/sme_fintinyF/run.sh
+ln -s ${PWD}/cmd_AM_sme_F.sh /scratch/work/jpleino1/kaldi-trunk/egs/est_LMs/cmd.sh
+ln -s ${PWD}/path_AM_sme_F.sh /scratch/work/jpleino1/kaldi-trunk/egs/est_LMs/path.sh
+ln -s ${PWD}/run_AM_sme_F.sh /scratch/work/jpleino1/kaldi-trunk/egs/est_LMs/run.sh
 
-#ln -s ${PWD}/cmd_AM_sme_fintinyF.sh /scratch/work/jpleino1/kaldi-trunk/egs/sme_LMs/cmd.sh
-#ln -s ${PWD}/path_AM_sme_fintinyF.sh /scratch/work/jpleino1/kaldi-trunk/egs/sme_LMs/path.sh
+#ln -s ${PWD}/cmd_AM_est_LMs.sh /scratch/work/jpleino1/kaldi-trunk/egs/sme_LMs/cmd.sh
+#ln -s ${PWD}/path_AM_est_LMs.sh /scratch/work/jpleino1/kaldi-trunk/egs/sme_LMs/path.sh
 #ln -s ${PWD}/run_LM_sme_LMs.sh /scratch/work/jpleino1/kaldi-trunk/egs/sme_LMs/run.sh
